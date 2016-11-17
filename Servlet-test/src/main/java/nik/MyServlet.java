c7fe6ed0c52fadd816a65ce8a12a0f08ad9ccd93
Nikas10 <nikas31415@gmail.com>
2016-11-17 16:19:20 +0300

    Create basic DB connection (SQLite), output DB metadata

--- Servlet-test/src/main/java/nik/MyServlet.java
+++ Servlet-test/src/main/java/nik/MyServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * Created by Nikas on 09.11.2016.
@@ -20,13 +24,31 @@ public class MyServlet extends HttpServlet {
     public MyServlet() {
         super();
     }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.print("<h1>Hello Servlet 1111111</h1>");
+        out.println("<h1>Hello Servlet 1111111</h1>");
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:G://JAVA DEV/ForumDB.sqlite";
+            Connection conn = DriverManager.getConnection(dbURL);
+            if (conn != null) {
+                out.println("Connected to the database");
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                out.println("Driver name: " + dm.getDriverName());
+                out.println("Driver version: " + dm.getDriverVersion());
+                out.println("Product name: " + dm.getDatabaseProductName());
+                out.println("Product version: " + dm.getDatabaseProductVersion());
+                conn.close();
+            }
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(out);
+        } catch (SQLException e) {
+            e.printStackTrace(out);
+        }
     }
-
-
-
 }
